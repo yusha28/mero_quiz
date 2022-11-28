4b3a4f498469a21bab3e887d4c96b745ac0f6907
Sonu0103 <sonuhp2002@gmail.com>
2022-11-28 20:57:37 +0545

    fetch data

--- src/view/login.java
+++ src/view/login.java
@@ -141,7 +141,10 @@ public class login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+ Reg jr3 = new Reg();
+       jr3.show();// TODO add your handling code here:
+       
+       dispose();                // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
